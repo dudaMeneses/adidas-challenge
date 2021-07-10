01465d41e71e6e7c740b2283a3cc5753e2d327a4
Eduardo Meneses <eduardo.demeneses@gmail.com>
2021-07-11 00:40:45 +0200

    commiting missing file

---
+++ src/main/kotlin/com/duda/adidaschallenge/application/rest/handler/ProductHandler.kt
@@ -0,0 +1,22 @@
+package com.duda.adidaschallenge.application.rest.handler
+
+import com.duda.adidaschallenge.application.projection.ProductResponse
+import com.duda.adidaschallenge.application.projection.request.ProductRequest
+import com.duda.adidaschallenge.domain.model.Product
+import com.duda.adidaschallenge.domain.service.ProductService
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+@Component
+class ProductHandler(private val productService: ProductService) {
+    fun sell(id: String, reservationToken: String) {
+        productService.sell(id, reservationToken)
+    }
+
+    fun create(request: ProductRequest): Mono<ProductResponse> =
+            productService.save(Product(name = request.name))
+            .map { product ->
+                ProductResponse(product.id, product.name)
+            }
+
+}
